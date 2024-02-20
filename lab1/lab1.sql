589dcfeec2e927f785be41b9d5c1760113933362
A1nzz <kmamcenko@gmail.com>
2024-02-20 18:28:57 +0300

    completed 3rd point

--- lab1/lab1.sql
+++ lab1/lab1.sql
@@ -3,6 +3,8 @@ CREATE TABLE MyTable (
   val NUMBER
 );
 
+select * from mytable
+
 DECLARE
   n INTEGER := 10000;
 BEGIN
@@ -16,3 +18,28 @@ BEGIN
   DBMS_OUTPUT.PUT_LINE('10 000 записей успешно добавлены.');
 END;
 /
+
+
+CREATE OR REPLACE FUNCTION CheckEvenOddCount RETURN VARCHAR2 IS
+  evenCount NUMBER := 0;
+  oddCount NUMBER := 0;
+BEGIN
+  SELECT COUNT(*) INTO evenCount
+  FROM MyTable
+  WHERE MOD(val, 2) = 0;
+
+  SELECT COUNT(*) INTO oddCount
+  FROM MyTable
+  WHERE MOD(val, 2) <> 0;
+
+  IF evenCount > oddCount THEN
+    RETURN 'TRUE';
+  ELSIF evenCount < oddCount THEN
+    RETURN 'FALSE';
+  ELSE
+    RETURN 'EQUAL';
+  END IF;
+END;
+/
+
+SELECT CheckEvenOddCount FROM dual;
